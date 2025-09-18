d204c8e44fff1d444ce44fbbd2aa12e63dd380d1
loynetgael <gael.loynet@gmail.com>
2025-09-18 16:43:48 +0200

    Corrections SonarCloud : variables séparées + condition redondante supprimée

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -64,7 +64,7 @@ public class ListeSimple {
     }
 
     public void supprimeTous(int element) {
-       tete = supprimeTousRecurs(element, tete);
+        tete = supprimeTousRecurs(element, tete);
     }
 
     public Noeud supprimeTousRecurs(Object element, Noeud tete) {
@@ -107,7 +107,7 @@ public class ListeSimple {
     }
 
     public Noeud getPrecedent(Noeud r) {
-    // la liste n'est pas vide puisqu'on transmet un Node de la liste et le Node existe obligatoirement
+        // la liste n'est pas vide puisqu'on transmet un Node de la liste et le Node existe obligatoirement
         Noeud precedent = tete;
         Noeud courant = precedent.getSuivant();
         while (courant != r) {
@@ -133,15 +133,14 @@ public class ListeSimple {
             precedentR2 = getPrecedent(r2);
             precedentR2.setSuivant(tete);
             tete = r2;
-        }
-        else if (r2 == tete) {
+        } else {  // correction : plus besoin de tester "r2 == tete"
             precedentR1 = getPrecedent(r1);
             precedentR1.setSuivant(tete);
             tete = r1;
         }
+
         Noeud temp = r2.getSuivant();
         r2.setSuivant(r1.getSuivant());
         r1.setSuivant(temp);
     }
-
 }
